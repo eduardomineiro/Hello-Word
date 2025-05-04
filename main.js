252e4b47c887f514d7199fa8323e7cad5c76f40d
Eduardo Mineiro.'. <62860830+eduardomineiro@users.noreply.github.com>
2025-05-04 07:44:18 -0300

    Construção do main.js
    Configurações e lógica em JavaScrip

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-let nome_usuário = prompt("Qual o seu nome?");
+let nomeUsuário = "";
 let elemento = document.querySelector("#nome-usuário");
-elemento.textContent = nome_usuário
+
+while (nomeUsuário == "") {
+    nomeUsuário = prompt("Qual o seu nome?");
+}
+
+if(nomeUsuário == null){
+    elemento.textContent = 'seja muito bem-vindo!';
+}else{
+    elemento.textContent = nomeUsuário;
+}
+
